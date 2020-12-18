b61e4578434aafeb6b22a0c59859a5e40c7920f8
Steve Engledow <steve@offend.me.uk>
2020-12-18 14:03:27 +0000

    Simplify part 2 after nicking ctd's ideas

--- 2020/18/part2.py
+++ 2020/18/part2.py
@@ -1,33 +1,14 @@
-import numpy
+import numpy, re
 
 data = open("input").read().strip().split("\n")
 
-def reduce(line):
-    parts = []
-    part = ""
-    level = 0
-    for i, a in enumerate(line):
-        part += a
-
-        if a == "(":
-            level += 1
-            if level == 1:
-                parts.append(part[:-1])
-                part = part[-1]
-        elif a == ")":
-            level -= 1
-            if level == 0:
-                parts.append(str(eval(part[1:-1])))
-                part = ""
-
-    parts.append(part)
-
-    return "".join(parts)
-
 def eval(line):
+    while "(" in line:
+        line = re.sub(r"\([^()]+\)", lambda m: str(eval(m.group(0)[1:-1])), line)
+
     return numpy.prod([
         sum([int(n) for n in part.split(" + ")])
-        for part in reduce(line).split(" * ")
+        for part in line.split(" * ")
     ])
 
 print(sum(eval(line) for line in data))
