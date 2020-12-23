1161b0e0b6752443d41b50b000e63d9d6953e14c
Steve Engledow <steve@offend.me.uk>
2020-12-23 12:11:28 +0000

    Add 2020/21

---
+++ 2020/21/part2.py
@@ -0,0 +1,58 @@
+allergens = {}
+ingredients = {}
+
+for line in open("input").read().strip().split("\n"):
+    ing, alg = line.split(" (contains ")
+
+    ing = ing.split(" ")
+    alg = alg[:-1].split(", ")
+
+    for i in ing:
+        if i not in ingredients:
+            ingredients[i] = 0
+
+        ingredients[i] += 1
+
+        for a in alg:
+            if a not in allergens:
+                allergens[a] = {}
+
+            if i not in allergens[a]:
+                allergens[a][i] = 0
+
+            allergens[a][i] += 1
+
+results = {}
+
+while any([
+    len(alg.keys()) > 1
+    for alg in allergens.values()
+]):
+
+    for key in results.keys():
+        if key in allergens:
+            del allergens[key]
+
+    for a, alg in allergens.items():
+        if a in results:
+            continue
+
+        choices = [
+            ing
+            for ing, count in alg.items()
+            if count == max(alg.values())
+        ]
+
+        if len(choices) == 1:
+            ing = choices[0]
+
+            results[a] = ing
+
+            for blg in allergens.values():
+                if ing in blg:
+                    del blg[ing]
+
+print(",".join([
+    ing
+    for alg, ing in sorted(results.items(), key=lambda pair: pair[0])
+]))
