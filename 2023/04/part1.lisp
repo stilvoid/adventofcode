c723b8c851eaf297d8f02d11d8fc5239d61866cd
Steve Engledow <steve@offend.me.uk>
2023-12-04 11:58:03 +0000

    Improve style

--- 2023/04/part1.lisp
+++ 2023/04/part1.lisp
@@ -5,25 +5,25 @@
         while line
         collect line)))
 
-(defun parse-entry (entry)
-  (let ((parts (str:split " | " entry)))
-    (list (parse-card (first parts)) (parse-nums (second parts)))))
+(defun parse-nums (nums)
+  (let ((parts (str:split " " nums :omit-nulls T)))
+    (mapcar 'parse-integer parts)))
 
 (defun parse-card (card)
   (let ((parts (str:split ": " card)))
     (parse-nums (second parts))))
 
-(defun parse-nums (nums)
-  (let ((parts (str:split " " nums :omit-nulls T)))
-    (mapcar 'parse-integer parts)))
+(defun parse-entry (entry)
+  (let ((parts (str:split " | " entry)))
+    (list (parse-card (first parts)) (parse-nums (second parts)))))
 
 (defvar *entries* (mapcar 'parse-entry *input*))
 
-(defun count-wins (entry)
-  (length (remove nil (mapcar (lambda (num) (check-win num (car entry))) (second entry)))))
-
 (defun check-win (num card)
   (find num card))
+  
+(defun count-wins (entry)
+  (length (remove nil (mapcar (lambda (num) (check-win num (car entry))) (second entry)))))
 
 (print (apply '+ (loop for entry in *entries* collect
   (let ((wins (count-wins entry)))
--- 2023/04/part2.lisp
+++ 2023/04/part2.lisp
@@ -5,25 +5,25 @@
         while line
         collect line)))
 
-(defun parse-entry (entry)
-  (let ((parts (str:split " | " entry)))
-    (list (parse-card (first parts)) (parse-nums (second parts)) 1)))
+(defun parse-nums (nums)
+  (let ((parts (str:split " " nums :omit-nulls T)))
+    (mapcar 'parse-integer parts)))
 
 (defun parse-card (card)
   (let ((parts (str:split ": " card)))
     (parse-nums (second parts))))
 
-(defun parse-nums (nums)
-  (let ((parts (str:split " " nums :omit-nulls T)))
-    (mapcar 'parse-integer parts)))
+(defun parse-entry (entry)
+  (let ((parts (str:split " | " entry)))
+    (list (parse-card (first parts)) (parse-nums (second parts)) 1)))
 
 (defvar *entries* (mapcar 'parse-entry *input*))
 
-(defun count-wins (entry)
-  (length (remove nil (mapcar (lambda (num) (check-win num (car entry))) (second entry)))))
-
 (defun check-win (num card)
   (find num card))
+
+(defun count-wins (entry)
+  (length (remove nil (mapcar (lambda (num) (check-win num (car entry))) (second entry)))))
 
 (loop for entry in *entries* for i from 1 do
   (let ((wins (count-wins entry)) (mult (third entry)))
