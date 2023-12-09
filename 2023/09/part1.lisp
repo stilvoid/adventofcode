87cff79bc8e4ecce0b94789b5ebced0cf5683545
Steve Engledow <steve@offend.me.uk>
2023-12-09 21:49:37 +0000

    2023-09.1

---
+++ 2023/09/part1.lisp
@@ -0,0 +1,22 @@
+(ql:quickload "str")
+
+(defvar *input*
+  (with-open-file (file "input")
+    (loop for line = (read-line file nil)
+          while line
+          collect line)))
+
+(setf *input* (loop for line in *input* collect
+                    (mapcar 'parse-integer (str:split " " line))))
+
+(defun diffs (seq)
+  (loop for a in (subseq seq 0 (- (length seq) 1)) 
+        for b in (subseq seq 1) collect
+        (- b a)))
+
+(defun get-next (seq)
+  (if (apply '= seq)
+    (first seq)
+    (+ (car (last seq)) (get-next (diffs seq)))))
+
+(print (apply '+ (mapcar 'get-next *input*)))
