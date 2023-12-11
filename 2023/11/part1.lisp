f81d036b7e4eb4a7f330f7783bf1a135a6b8cd14
Steve Engledow <steve@offend.me.uk>
2023-12-11 17:06:51 +0000

    2023-11.1 better

--- 2023/11/part1.lisp
+++ 2023/11/part1.lisp
@@ -9,50 +9,40 @@
 (setf *input* (loop for line in *input* collect
                     (loop for c across line collect c)))
 
-(defvar *universe* nil)
+; Determine the size of space
+(defvar *space* (loop for line in *input* collect
+                      (let ((h 1))
+                        (when (apply 'char= (append (list #\.) line)) (setf h 2))
+                        (loop for i from 0 to (- (length line) 1) collect
+                              (let ((w 1))
+                              (let ((col (mapcar (lambda (u) (elt u i)) *input*)))
+                                (when (apply 'char= (append (list #\.) col)) (setf w 2))
+                                (list w h)))))))
 
-; Expand horizontal space
-(loop for line in *input* do
-    (when (apply 'char= (append (list #\.) line)) (push line *universe*))
-    (push line *universe*))
-
-(setf *universe* (reverse *universe*))
-
-(loop for line in *input* do (print (coerce line 'string)))
-
-(print "")
-
-(loop for line in *universe* do (print (coerce line 'string)))
-
-; Expand vertical space
-(defvar *cols* (loop for i from 0 to (- (length (first *universe*)) 1) collect
-      (let ((col (mapcar (lambda (line) (elt line i)) *universe*)))
-        (when (apply 'char= (append (list #\.) col)) T))))
-
-(print *cols*)
-
-(setf *universe* (loop for line in *universe* collect
-      (let ((out nil))
-          (loop for c in line for i from 0 do
-                (when (elt *cols* i) (push c out))
-                (push c out))
-          (reverse out))))
+(loop for line in *space* do (print line))
 
 (print "")
 
-(loop for line in *universe* do (print (coerce line 'string)))
-
-(print "")
-
-(setf *galaxies* (apply 'append (loop for line in *universe* for y from 0 collect
-    (remove nil (loop for c in line for x from 0 collect
-      (when (char= c #\#) (list x y)))))))
+(defvar *galaxies* (apply 'append (loop for line in *input* for y from 0 collect
+                         (remove nil (loop for c in line for x from 0 collect
+                               (when (char= c #\#) (list x y)))))))
 
 (print *galaxies*)
 
-(print "")
+(loop for galaxy in *galaxies* do
+      (setf galaxy "potato"))
 
-(print (reduce '+ (loop for g1 in *galaxies* for i from 0 collect
-      (reduce '+ (remove nil (loop for j from (+ i 1) to (- (length *galaxies*) 1) collect
-                                   (let ((g2 (elt *galaxies* j)))
-            (when (not (= i j)) (+ (abs (- (first g1) (first g2))) (abs (- (second g1) (second g2))))))))))))
+(defun resolve (galaxy)
+  (let ((gx 0) (gy 0))
+    (loop for x from 0 to (- (first galaxy) 1) do (setf gx (+ gx (first (elt (elt *space* (second galaxy)) x)))))
+    (loop for y from 0 to (- (second galaxy) 1) do (setf gy (+ gy (second (elt (elt *space* y) (first galaxy))))))
+    (list gx gy)))
+
+(setf *galaxies* (mapcar 'resolve *galaxies*))
+
+(print *galaxies*)
+
+(print (apply '+ (loop for g1 in *galaxies* for i from 0 collect
+             (apply '+ (loop for j from (+ i 1) to (- (length *galaxies*) 1) collect
+                   (let ((g2 (elt *galaxies* j)))
+                     (+ (abs (- (first g1) (first g2))) (abs (- (second g1) (second g2))))))))))
