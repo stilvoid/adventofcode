9568e8aede9dfc4902b31462b3b447ef4dcb9f95
Steve Engledow <steve@offend.me.uk>
2025-12-02 13:14:40 +0000

    2025.02

---
+++ 2025/02/part1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+int check(char* subject) {
+    int len = strlen(subject) / 2;
+
+    if(strlen(subject) == len * 2) {
+        int size = strlen(subject) / len;
+        int match = -1;
+
+        for(int start=len; start<strlen(subject); start+=len) {
+            if(strncmp(subject, subject+start, len) != 0) {
+                match = 0;
+            }
+        }
+
+        if(match) {
+            //printf("%s :: %d\n", subject, len);
+            return match;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc!=2) {
+        fprintf(stderr, "Need INPUT\n");
+        return 1;
+    }
+
+    FILE* f = fopen(argv[1], "r");
+
+    int max = 256;
+    int n;
+    char buf[max];
+
+    unsigned long result = 0;
+
+    while(!feof(f)) {
+        unsigned long start, end;
+
+        fscanf(f, "%lu-%lu", &start, &end);
+
+        for(unsigned long i=start; i<=end; i++) {
+            n = snprintf(buf, max, "%lu", i);
+            if(n>=max) {
+                fprintf(stderr, "number too long!\n");
+                return 1;
+            }
+
+            if(check(buf)) {
+                result += i;
+            }
+        }
+
+        if(!feof(f)) {
+            char c = fgetc(f);
+            if(c != ',') {
+                break;
+            }
+        }
+    }
+
+    printf("RESULT: %lu\n", result);
+
+    fclose(f);
+
+    return 0;
+}
